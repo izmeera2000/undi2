fd414d8ffb1b820e7b87388b4f938a62ea4046fa
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2026-01-30 23:13:37 +0800

    asd
    asdas

--- app/Models/PengundiRaw.php
+++ app/Models/PengundiRaw.php
@@ -9,11 +9,31 @@ class PengundiRaw extends Model
     protected $table = 'pengundi_raw';
 
     protected $fillable = [
-        'kod_par','nama_par','kod_dun','nama_dun','koddm','namadm',
-        'kod_lokaliti','nama_lokaliti','nokp_baru','nokp_lama','nama',
-        'alamat_spr','bangsa','bangsa_spr','jantina','status_baru',
-        'kodpar_pru12','tahun_lahir','umur','status_umno',
-        'alamat_jpn1','alamat_jpn2','alamat_jpn3',
-        'poskod','bandar','negeri'
+        'kod_par',
+        'namapar',
+        'kod_dun',
+        'namadun',
+        'koddm',
+        'namadm',
+        'kodlokaliti',
+        'namalokaliti',
+        'nokp_baru',
+        'nokp_lama',
+        'nama',
+        'alamat_spr',
+        'bangsa',
+        'bangsa_spr',
+        'jantina',
+        'status_baru',
+        'kodpar_pru12',
+        'tahun_lahir',
+        'umur',
+        'status_umno',
+        'alamat_jpn_1',
+        'alamat_jpn_2',
+        'alamat_jpn_3',
+        'poskod',
+        'bandar',
+        'negeri',
     ];
 }
